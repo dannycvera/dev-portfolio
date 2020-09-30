32b3af06b00a5f243d55b88efc22e70dccd95c99
danv <dannyvera@gmail.com>
2020-09-30 14:24:38 -0400

    fixed image sizes and manually installed icons

--- src/screens/Skills.jsx
+++ src/screens/Skills.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Skills.css";
-import "devicon/devicon-colors.css";
-import "devicon/devicon.css";
 
 function Skills() {
   return (
@@ -9,21 +7,76 @@ function Skills() {
       <div className="skills-wrapper">
         <h1 className="skills-title">Skills</h1>
         <div className="skills-icons">
-          <i className="devicon-postgresql-plain-wordmark colored"></i>
-          <i className="devicon-rails-plain-wordmark colored"></i>
-          <i className="devicon-ruby-plain-wordmark colored"></i>
-          <i className="devicon-mongodb-plain-wordmark colored"></i>
-          <i className="devicon-express-original colored"></i>
-          <i className="devicon-nodejs-plain-wordmark colored"></i>
-          <i className="devicon-heroku-plain-wordmark colored"></i>
-          <i className="devicon-react-original-wordmark colored"></i>
-          <i className="devicon-javascript-plain colored"></i>
-          <i className="devicon-bootstrap-plain-wordmark colored"></i>
-          <i className="devicon-jquery-plain-wordmark colored"></i>
-          <i className="devicon-css3-plain-wordmark colored"></i>
-          <i className="devicon-html5-plain-wordmark colored"></i>
-          <i className="devicon-github-plain-wordmark colored"></i>
-          {/* <i className="devicon-photoshop-line colored"></i> */}
+          <img
+            className="skills-icon"
+            src={require("../img/postgresql-plain-wordmark.svg")}
+            alt="postgresql"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/rails-plain-wordmark.svg")}
+            alt="rails"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/ruby-plain-wordmark.svg")}
+            alt="ruby"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/mongodb-plain-wordmark.svg")}
+            alt="mongodb"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/express-original-wordmark.svg")}
+            alt="express"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/nodejs-plain-wordmark.svg")}
+            alt="nodejs"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/heroku-plain-wordmark.svg")}
+            alt="heroku"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/react-original-wordmark.svg")}
+            alt="react"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/javascript-plain.svg")}
+            alt="javascript"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/bootstrap-plain-wordmark.svg")}
+            alt="bootstrap"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/jquery-plain-wordmark.svg")}
+            alt="jquery"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/css3-plain-wordmark.svg")}
+            alt="css3"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/html5-plain-wordmark.svg")}
+            alt="html5"
+          />
+          <img
+            className="skills-icon"
+            src={require("../img/github-original-wordmark.svg")}
+            alt="github"
+          />
         </div>
       </div>
     </div>
